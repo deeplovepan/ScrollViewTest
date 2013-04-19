2afff3cc20f4e56012ce160f089e061fc528323a
Peter Pan <PeterPan@Peters-MacBook-Pro.local>
2013-04-20 01:47:55 +0800

    forceBounce

--- ScrollViewTest/ViewController.m
+++ ScrollViewTest/ViewController.m
@@ -23,18 +23,11 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    self.testScrollView.contentSize = CGSizeMake(320*3, 460*3);
     
     UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(10, 10, 1014, 762)];
     imageView.image = [UIImage imageNamed:@"book.jpg"];
     [self.testScrollView addSubview:imageView];
     
-    UITextView *textView = [[UITextView alloc] initWithFrame:
-                            CGRectMake(350, 500, 220, 100)];
-    textView.text = @"希臘英雄也會寫App\n寫App的果然都是英雄";
-    textView.font = [UIFont systemFontOfSize:20];
-    [self.testScrollView addSubview:textView];
-    textView.scrollsToTop = NO;
 }
 
 - (void)didReceiveMemoryWarning
